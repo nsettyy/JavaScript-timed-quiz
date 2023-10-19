8a08d63f8e28f326e151642a3e9d7db60fab11df
nsetty <nsett7388@gmail.com>
2023-10-19 15:56:31 -0400

    added quiz questions

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 //global variables
+var startButtonEl = document.querySelector.getElementById("start-button");
+var homeEl = document.querySelector.getElementById("home");
 
-//timer for quiz that is one minute long, and subtracts 5 seconds for every wrong answer
-
-//5 quiz questions
+//quiz questions
+const questions = [
+  {
+    question: "What is the main purpose of JavaScript?",
+    answers: [
+      "To style a webpage",
+      "To make webpages intercative",
+      "To watch videos",
+      "To create a webpage",
+    ],
+    correctIndex: 1,
+  },
+  {
+    question: "How do you select an Id within JavaScript?",
+    answers: ["selectId", "getId", "querySelector", "websiteSelector"],
+    correctIndex: 2,
+  },
+  {
+    question: "Approximately when was JavaScript invented?",
+    answers: ["1985", "1995", "2005", "2015"],
+    correctIndex: 1,
+  },
+  {
+    question: "What is the JavaScript file called?",
+    answers: ["style.js", "index.js", "js.jpeg", "script.js"],
+    correctIndex: 3,
+  },
+  {
+    question: "How do you declare a variable in JavaScript?",
+    answers: [
+      "var name",
+      "variable name",
+      "createVariable name",
+      "declareVariable name",
+    ],
+    correctIndex: 0,
+  },
+];
